479b030d2664e0b6d38814b2c99748e29fd044e7
Emma Fuller <efuller@princeton.edu>
2014-03-12 09:06:50 -0400

    Incorporating Malin's edits

--- alt_fish_figures.R
+++ alt_fish_figures.R
@@ -1,22 +1,17 @@
 # for synced online github data
-x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAnull_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQW51bGxfMjAxNC0wMy0wMi5jc3YiLCJleHBpcmVzIjoxMzk0NDc3MzYwfQ%3D%3D--5fa73caaafffae7c95d930f61def04ee9b96930a")
-sim = read.csv(textConnection(x))
-
-fish = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAfish_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQWZpc2hfMjAxNC0wMy0wMi5jc3YiLCJleHBpcmVzIjoxMzk0NDc3MzQ2fQ%3D%3D--9b6ceb0092173222543c95381dfbb0f42f0c884f")
-fishmpa = read.csv(textConnection(fish))
-
-cons = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAcons_2014-03-01.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQWNvbnNfMjAxNC0wMy0wMS5jc3YiLCJleHBpcmVzIjoxMzk0NDc3MzIwfQ%3D%3D--5f4f584256a4175eebe2ef5465e83c92ebe65cc0")
-consmpa = read.csv(textConnection(cons))
-
-thresh = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/Thresh_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL1RocmVzaF8yMDE0LTAzLTAyLmNzdiIsImV4cGlyZXMiOjEzOTQ0ODIyMTV9--13d78257cda683d18eb8ecc71dcf0f4e6101b41d")
-threshz = read.csv(textConnection(thresh))
+source("load_data.R")
+require(lattice)
+require(RColorBrewer)
+threshz = thresh
+fishmpa = mpas
+consmpa = mpas2
 
 # combine all data into one dataframe
 sim$management = rep("No MPAs",nrow(sim))
 consmpa$management = rep("Few Large MPAs", nrow(consmpa))
 fishmpa$management = rep("Many Small MPAs", nrow(fishmpa))
 threshz$management = rep("Thresholds", nrow(threshz))
-allData <- rbind(consmpa,fishmpa)
+allData <- rbind(consmpa,fishmpa, sim)
 
 
 # lattice plot settings
@@ -39,7 +34,7 @@ lattice.options(layout.widths = lw, layout.heights = lh)
 # mpa plots
 
 pdf(file="Writing/plots/MPAs.pdf",width=3,height=5)
-contourplot(Equil.pop ~ speed * harvest | factor(management), data = allData, region=TRUE, layout = c(1,2), drape=TRUE,col.regions=rev(colorRampPalette(brewer.pal(9,"Greys"))(100)),colorkey=FALSE,labels=list(cex=.75,col="white"))
+contourplot(Equil.pop ~ speed * harvest | factor(management), data = allData, region=TRUE, layout = c(2,2), drape=TRUE,col.regions=rev(colorRampPalette(brewer.pal(9,"Greys"))(100)),colorkey=FALSE,labels=list(cex=.75,col="white"))
 dev.off()
 
 #threshold plot
@@ -47,3 +42,10 @@ dev.off()
 pdf(file="Writing/plots/Threshold.pdf",width=3,height=3)
 contourplot(Equil.pop ~ speed * -thresh | factor(management), data = threshz, region=TRUE, drape=TRUE,col.regions=rev(colorRampPalette(brewer.pal(9,"Greys"))(100)),colorkey=FALSE,labels=list(cex=.75,col="white"),scales=list( y=list(at=seq(-8,0,2),labels=round(rev(unique(threshz$thresh)/max(threshz$thresh)),3))),ylab=list(label="proportion below threshold"))
 dev.off()
+
+### ggplots
+require(ggplot2)
+require(ggthemes)
+
+ggplot(allData, aes(x = speed, y = harvest, fill = Equil.pop)) + geom_tile() + facet_wrap(~management, scale="free") + geom_tile(data = threshz, aes(x = speed, y = rev(thresh), fill = Equil.pop)) + theme_tufte() + scale_fill_gradient(low="black", high="white") + theme(legend.position="bottom", text=element_text(family="Helvetica", size = 14)) + guides(fill = guide_colorbar(barwidth = 18, barheight = 1, title.position="top", title="Equilibrium Biomass"))
+
--- load_data.R
+++ load_data.R
@@ -3,20 +3,20 @@ library(RCurl)
 
 # analytics
 
-x=getURL("https://raw.github.com/emfuller/MovingFish/d6b73bca90c71f0aedb616a241df24b536e0f80b/eqbiomass_gaus.csv?token=6224444__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9kNmI3M2JjYTkwYzcxZjBhZWRiNjE2YTI0MWRmMjRiNTM2ZTBmODBiL2VxYmlvbWFzc19nYXVzLmNzdiIsImV4cGlyZXMiOjEzOTQ1NTkzNjF9--9f57d5392b49fe11ef00599873ecc6b66926ba10")
+x=getURL("https://raw.github.com/emfuller/MovingFish/master/eqbiomass_gaus.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvZXFiaW9tYXNzX2dhdXMuY3N2IiwiZXhwaXJlcyI6MTM5NTE4NTQ4N30%3D--65033562cc7e8c52f04dbf0ea06cae97e4008776")
 ebm=read.csv(textConnection(x),row.names=1)
 ebm=as.matrix(ebm,nrow=length(ebm[,1]))
 
 # simulations
 
-x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAnull_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQW51bGxfMjAxNC0wMy0wMi5jc3YiLCJleHBpcmVzIjoxMzk0NDA4NjYyfQ%3D%3D--7f380bfb17f1cd58ff4f131134ed434700767def")
+x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAnull_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQW51bGxfMjAxNC0wMy0wMi5jc3YiLCJleHBpcmVzIjoxMzk1MTg1MzA2fQ%3D%3D--8e3f018b7a2cdc64244831017565809335d2e160")
 	sim = read.csv(textConnection(x),row.names=1)
 
-	x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAfish_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQWZpc2hfMjAxNC0wMy0wMi5jc3YiLCJleHBpcmVzIjoxMzk0NDA4NjQxfQ%3D%3D--2dba33c2046114811967e01914fc795e1771f425")
-	mpas = read.csv(textConnection(x))
+	x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAfish_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQWZpc2hfMjAxNC0wMy0wMi5jc3YiLCJleHBpcmVzIjoxMzk1MTg1MjYzfQ%3D%3D--78007e9dd447a7c3232f532eae1e0b82e0037767")
+	mpas = read.csv(textConnection(x), row.names=1)
 
-x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAcons_2014-03-04.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQWNvbnNfMjAxNC0wMy0wNC5jc3YiLCJleHBpcmVzIjoxMzk0NTU5MTY0fQ%3D%3D--dbd27e32e8632468f8c1113dbd4b470bba84a535")
-	mpas2 = read.csv(textConnection(x))
+x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/MPAcons_2014-03-04.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL01QQWNvbnNfMjAxNC0wMy0wNC5jc3YiLCJleHBpcmVzIjoxMzk1MTg0OTk5fQ%3D%3D--ec3e0f05373e64ef2f192b2ad265ca061749c738")
+	mpas2 = read.csv(textConnection(x), row.names=1)
 
-x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/Thresh_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL1RocmVzaF8yMDE0LTAzLTAyLmNzdiIsImV4cGlyZXMiOjEzOTQ0MDg2OTF9--50c2acb25198937b8f95463cd2713c2db8f124b2")
-	thresh = read.csv(textConnection(x))
+x = getURL("https://raw.github.com/emfuller/MovingFish/master/Simluations/Aspatial_fast/Data/Thresh_2014-03-02.csv?token=3235371__eyJzY29wZSI6IlJhd0Jsb2I6ZW1mdWxsZXIvTW92aW5nRmlzaC9tYXN0ZXIvU2ltbHVhdGlvbnMvQXNwYXRpYWxfZmFzdC9EYXRhL1RocmVzaF8yMDE0LTAzLTAyLmNzdiIsImV4cGlyZXMiOjEzOTUxODUzNzR9--c90173573d965821e9a6976a8f7be242d14544f5")
+	thresh = read.csv(textConnection(x), row.names=1)
